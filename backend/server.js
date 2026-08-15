82f61b4095289eb696b8246e6c7e07b9131b3e03
Priyanka B <priyanka042007@gmail.com>
2026-08-15 15:49:46 +0530

    fix server port for Render

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const projectRoutes = require("./routes/projectRoutes");
 const contactRoutes = require("./routes/contactRoutes");
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
